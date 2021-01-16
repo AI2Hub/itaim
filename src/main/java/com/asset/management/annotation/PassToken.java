d9ee55ef2acc7a5ccaf937aca0df4697e13e1dde
wenchiji <1565699710@qq.com>
2021-01-16 23:36:51 +0800

    token模块-未完

--- src/main/java/com/asset/management/annotation/PassToken.java
+++ src/main/java/com/asset/management/annotation/PassToken.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 用来跳过验证的 PassToken
+ */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PassToken {
--- src/main/java/com/asset/management/annotation/UserLoginToken.java
+++ src/main/java/com/asset/management/annotation/UserLoginToken.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 用于登录后才能操作的token
+ */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UserLoginToken {
